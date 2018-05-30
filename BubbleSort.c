b0eaa5c411afa8f597ab4be68c4d3dcc46050729
ruijiehuang <qjjisamonkey@gmail.com>
2018-05-31 01:04:40 +0800

    Ver 1.00
    non-constant pointer to non-constant data

---
+++ BubbleSort.c
@@ -0,0 +1,27 @@
+#define SIZE 10
+//sort an array of integers using bubble sort algorithm
+void bubbleSort(int * array, unsigned int size )
+	{
+		void swap(int *element1Ptr, int *element2Ptr );
+		unsigned int pass;//pass counter
+		unsigned int j;//comparison counter
+	//loop to control passes	
+	for (pass= 0;pass<SIZE;++pass){
+		//loop to control comparisons during each pass
+	    for(j=0;j<size-1;++j){
+	    	    //swap adjacent elements if theyre out of order
+				if(array[j]>array[j+1]){
+					swap(&array[j],&array[j+1]);		
+				}//end if
+			}//end inner for
+		}//end outer for
+    }//end function bubbleSort
+    
+//swap values at memory locations to which element1Ptr and
+//element2Ptr point
+void swap(int *element1Ptr,int *element2Ptr)
+{
+	int hold=*element1Ptr;
+	*element1Ptr=*element2Ptr;
+	*element2Ptr= hold;
+}//end function swap
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void bubbleSort(int * array, unsigned int size ); //prototype
+
+int main(void) {
+	//initialize array a
+	int a[SIZE]={245,3,97,85,657,156,87,17,32,222};
+	
+	unsigned int i; //counter
+	
+	puts(" Data items in original order");
+	    //loop through array a
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+		}//end for
+		
+		bubbleSort(a, SIZE); //sort the array
+		
+		puts("\n Data items in ascending order");
+		//loop through array a
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+	    }//end for
+		
+		puts("");
+}//end main
